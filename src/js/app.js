373539d807a70a042e7c1022ed445167bd309eb1
Malin Fossum <malinfossum.dev@proton.me>
2026-03-04 21:56:56 +0100

    refactor: remove if validation to match parameters/functions assignment focus

--- src/js/app.js
+++ src/js/app.js
@@ -14,23 +14,14 @@ function showBackgroundColor(value) {
 }
 
 function buttonUpdate() {
-	if (userName === "") {
-		output = "Please enter a name.";
-		updateView();
-		return;
-	}
-
-	if (userAge === "") {
-		output = "Please enter a valid age.";
-		updateView();
-		return;
-	}
-
 	output = `
 	<br>
 	Name: ${userName}<br>
 	Age: ${userAge}<br>
-	Favorite color: ${favoriteColor}`;
+	Favorite color: ${favoriteColor}
+	`;
+
 	document.body.style.backgroundColor = favoriteColor;
+
 	updateView();
 }
